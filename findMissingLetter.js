a0fc2041ecdcfcc3743e5b079ac36226294c77be
Piotr Skwarczynski <piotrskwarczynski184@gmail.com>
2022-08-02 11:35:54 +0200

    n/a

--- findMissingLetter.js
+++ findMissingLetter.js
@@ -1,12 +1,19 @@
 //Write a method that takes an array of consecutive (increasing) letters as input and that returns the missing letter in the array.
 
+//CodeWars tests keep failing but I'm pretty sure it should work
+
 function findMissingLetter(array)
 {
     const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    let difference = alphabet.toLowerCase().split('').slice(alphabet.indexOf(array.join('').toUpperCase()[0]), array.length + 1).filter(x => array.join('').toLowerCase().split('').indexOf(x) === -1)
-    return difference
+    let indexFirst = alphabet.indexOf(array.join('').toUpperCase()[0])
+    let isUpper = array => array.join('').toUpperCase() == array.join('') //not necessary but I wanted to experiment a bit 
+    let difference = alphabet.toLowerCase().split('').slice(indexFirst, indexFirst + array.length).filter(x => array.join('').toLowerCase().split('').indexOf(x) === -1)
+    if (array.length == 1) {
+        return array[0]
+    }
+    return difference.join('')
 }
 
-array = ['e']
+array = ['p']
 console.log(findMissingLetter(array))
 
